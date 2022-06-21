722bbd87d615dc80b1cbf3959557c37e26799476
Vito Farenga <vitofarenga@libero.it>
2022-06-21 15:49:56 +0200

    ciclo while java

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-const cart = ['burro', 'lievito', 'pane', 'nutella', 'motosega']
+const cart = ['burro', 'lievito', 'pane', 'nutella', 'motosega'];
+
+
+let i = 0;
+
+while ( i < (cart.length)){
+     console.log(cart[i]);
+    i++;
+}
 
