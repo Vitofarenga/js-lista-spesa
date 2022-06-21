cefd68d1425d1ad89f7528226fdf70a1293534cd
Vito Farenga <vitofarenga@libero.it>
2022-06-21 16:07:09 +0200

    codice modificato non funzionante non capisco

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const cart = ['burro', 'lievito', 'pane', 'nutella', 'motosega'];
 
-
+const cartElement = document.getElementById('carrello');
+console.log(cartElement);
 let i = 0;
 
 while ( i < (cart.length)){
-     console.log(cart[i]);
-    i++;
+    const elementoSpesa = document.createElement('li');
+    elementoSpesa.innerText = cart[i];
+    cartElement.append(elementoSpesa);
+     i++
+    
 }
+
 
